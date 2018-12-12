3d282bb5f87b1551c48b6729c31bcda301e524be
aonemd <aonemdsaleh@gmail.com>
2018-12-12 14:31:27 +0200

    Add archive/unarchive command

--- lib/nin/command.rb
+++ lib/nin/command.rb
@@ -20,16 +20,19 @@ module Nin
         @todo.edit(@args[0].to_i, desc, date, tags)
       when 'c'
         @todo.complete(@args[0].to_i)
+      when 'ac'
+        @todo.archive(@args[0].to_i)
       when 'd'
         @todo.delete(@args[0].to_i)
       else
         puts "\nUSAGE: nin COMMAND [arguments...]\n\n"
         puts "COMMANDS:"
-        puts "  l [a]         List all unarchived todos. Pass optional argument `a` to list all todos"
-        puts "  a desc        Add a todo"
-        puts "  e id desc     Edit a todo"
-        puts "  c id          Un/complete a todo"
-        puts "  d id          Remove a todo"
+        puts "  l  [a]         List all unarchived todos. Pass optional argument `a` to list all todos"
+        puts "  a  desc        Add a todo"
+        puts "  e  id desc     Edit a todo"
+        puts "  c  id          Un/complete a todo"
+        puts "  ac id          Un/archive a todo"
+        puts "  d  id          Remove a todo"
       end
     end
 
